12b321c3bb03ff5ad187f6de4c2e9338eb4e4828
DawidCyga <dawid.zagula23@gmail.com>
2024-05-27 12:08:26 +0200

    feat: add audio manager, add audio mixer, create groups for master, sfx, music and expose volume parameters

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance { get; private set; }
+
+    public enum AudioName
+    {
+        // This enum will contain explicitly declared enumerator list. 
+        //The elements values correspond to positions of audio clips in audio clip list
+        //Enum elements are keys in enumNameAudioDictionary
+        // Manager will subscribe to events triggered on objects producing sounds
+    }
+
+
+    [SerializeField] private List<AudioClip> _audioClipList = new List<AudioClip>();
+
+    private Dictionary<AudioName, AudioClip> _enumNameAudioClipDictionary = new Dictionary<AudioName, AudioClip>();
+
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _audioSource = GetComponent<AudioSource>();
+
+        foreach (AudioName audioName in System.Enum.GetValues(typeof(AudioName)))
+        {
+            _enumNameAudioClipDictionary[audioName] = _audioClipList[(int)audioName];
+        }
+
+    }
+
+    private void PlaySound(AudioName name)
+    {
+        _audioSource.PlayOneShot(_enumNameAudioClipDictionary[name]);
+    }
+}
